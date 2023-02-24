08437bd6b4a89c32ada895d8f43aadc413c00bba
Duerme <105309906+Duerme@users.noreply.github.com>
2023-02-24 14:36:08 -0500

    Update main.go
    GoSpy v0.0.1

--- main.go
+++ main.go
@@ -14,6 +14,24 @@ import (
 )
 
 func main() {
+
+	colorGreen := "\033[32m"
+	colorReset := "\033[0m"	
+	fmt.Println(string(colorGreen),`
+	
+ .d8888b.            .d8888b.                    
+d88P  Y88b          d88P  Y88b                   
+888    888          Y88b.                        
+888         .d88b.   "Y888b.   88888b.  888  888 
+888  88888 d88""88b     "Y88b. 888 "88b 888  888 
+888    888 888  888       "888 888  888 888  888 
+Y88b  d88P Y88..88P Y88b  d88P 888 d88P Y88b 888 
+ "Y8888P88  "Y88P"   "Y8888P"  88888P"   "Y88888 
+			       888           888 
+			       888      Y8b d88P 
+			       888       "Y88P"`,string(colorReset))
+
+	fmt.Print("\n\n")
 	valInput := false
 	reader := bufio.NewReader(os.Stdin) // Initialize user input reader
 	var websiteAddress string
